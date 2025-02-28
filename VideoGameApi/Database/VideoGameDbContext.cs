c3bdc3ac94e312ef8de9b7e154e51d9b2ae9de9a
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-28 19:24:11 +0300

    Database Connection

---
+++ VideoGameApi/Database/VideoGameDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Entites;
+
+namespace VideoGameApi.Database
+{
+    public class VideoGameDbContext:DbContext
+    {
+        public VideoGameDbContext(DbContextOptions<VideoGameDbContext>options):base(options)
+        {
+    
+        }
+
+        public VideoGameDbContext()
+        {
+
+        }
+
+
+
+        public DbSet<VideoGame> VideoGames { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
--- VideoGameApi/Program.cs
+++ VideoGameApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//builder.Services.AddDbContext<VideoGameContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<VideoGameDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
