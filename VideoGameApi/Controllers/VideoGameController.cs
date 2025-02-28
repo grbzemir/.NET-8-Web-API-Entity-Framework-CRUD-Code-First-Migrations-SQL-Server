54297c03237422c95f328593014f82a9d7ea2622
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-28 19:16:30 +0300

    FakeApi And Controllers Codes

--- VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -47,6 +47,10 @@ namespace VideoGameApi.Controllers
             return Ok(videoGames);
         }
 
+        [HttpGet]
+        //Route ile urller belirleniyor.
+        //id ile gelen parametreleri alıyoruz.
+        [Route("{id}")]
         public ActionResult<VideoGame> GetVideoGameById(int id) 
         {
             var videoGame = videoGames.FirstOrDefault(v => v.Id == id);
@@ -55,6 +59,57 @@ namespace VideoGameApi.Controllers
                 return NotFound();
             }
             return Ok(videoGame);
+        }
+
+        [HttpPost]
+
+        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame == null)
+            {
+                return BadRequest();
+            }
+
+            newGame.Id = videoGames.Max(v => v.Id) + 1;
+            videoGames.Add(newGame);
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+
+        }
+
+        [HttpPut("{id}")]
+        
+
+        public IActionResult UpdateVideoGame(int id , VideoGame updatesGame)
+
+        {
+            var game = videoGames.FirstOrDefault(v => v.Id == id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            game.Title = updatesGame.Title;
+            game.Platform = updatesGame.Platform;
+            game.Developer = updatesGame.Developer;
+            game.Publisher = updatesGame.Publisher;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteVideoGame(int id)
+        {
+            var game = videoGames.FirstOrDefault(v => v.Id == id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            videoGames.Remove(game);
+            return NoContent();
         }
     }
 }
--- VideoGameApi/Program.cs
+++ VideoGameApi/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<VideoGameContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<VideoGameContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
