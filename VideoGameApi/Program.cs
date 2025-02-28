c51d5c9c82126a8122117b76a0eb26bc3c92edb9
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-28 18:40:38 +0300

    Database Settings

--- VideoGameApi/Program.cs
+++ VideoGameApi/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<VideoGameContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
