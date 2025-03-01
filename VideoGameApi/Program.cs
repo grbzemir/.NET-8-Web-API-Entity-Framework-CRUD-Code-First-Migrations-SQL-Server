be29707864c53a18840567426e9ffd62b809114a
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-01 12:29:54 +0300

    SqlConnection

--- VideoGameApi/Program.cs
+++ VideoGameApi/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddDbContext<VideoGameDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
